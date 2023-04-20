a057f0b43383cb048520f96ed8fe87576449db51
Walace Abreu <gbwalace@gmail.com>
2023-04-20 08:50:47 -0300

    first commit

---
+++ alurapic/src/app/photos/photo-list/search/search.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
+@Component({
+  selector: 'ap-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit, OnDestroy {
+
+  @Output() onTyping: EventEmitter<string> = new EventEmitter<string>();
+  @Input() value: String = '';
+
+  debounce: Subject<string> = new Subject<string>();
+
+  ngOnInit(): void {
+    this.debounce.pipe(debounceTime(300)).subscribe(filter => this.onTyping.emit(filter));
+  }
+
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
+  }
+
+}
